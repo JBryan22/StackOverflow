5661c801c398ff1979cff6e472162de2ea2aa09b
Guest User <Guest@epicodus-50.local>
2017-10-24 08:18:38 -0700

    initial commit

---
+++ StackOverflow/Models/Answer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StackOverflow.Models
+{
+    public class Answer
+    {
+        [Key]
+        public int AnswerId { get; set; }
+        [Required]
+        public string Answer_Body { get; set; }
+        public bool Best_Answer { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
---
+++ StackOverflow/Models/Question.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace StackOverflow.Models
+{
+    public class Question
+    {
+        [Key]
+        public int QuestionId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Content_Body { get; set; }
+        public bool Is_Answered { get; set; }
+        public DateTime Submission_Date { get; set; }
+
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
+    }
+}
